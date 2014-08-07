ad6a27d5d190e487fdf7e62aa9dfe4b2fca419bf
Masato Ikeda <masato.ikeda@gmail.com>
2014-08-08 02:48:20 +0900

    Reply to messages which haruna can't understand

--- handlers/reply.rb
+++ handlers/reply.rb
@@ -6,6 +6,13 @@ module Lita
         return unless message.user.metadata["mention"]
         message.reply_with_mention "榛名は大丈夫です！"
       end
+
+      on :unhandled_message, :i_cant_understand
+      def i_cant_understand(payload)
+        message = payload[:message]
+        return unless message.user.metadata["mention"]
+        message.reply "@#{message.user.name} すみません、なにをおっしゃってるかわかりません…。提督、大丈夫ですか…？"
+      end
     end
 
     Lita.register_handler(Reply)
