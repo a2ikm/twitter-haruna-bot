1f33c81b04fd734d1c718ed6095553701cfd322d
Masato Ikeda <masato.ikeda@gmail.com>
2014-08-08 02:24:53 +0900

    Cosmetic

--- handlers/reply.rb
+++ handlers/reply.rb
@@ -1,9 +1,8 @@
 module Lita
   module Handlers
     class Reply < Handler
-      route /大丈夫？/, :all_right
-
-      def all_right(message)
+      route /大丈夫？/, :all_right!
+      def all_right!(message)
         return unless message.user.metadata["mention"]
         message.reply "@#{message.user.name} 榛名は大丈夫です！"
       end
