15904c9e55a87596d6dd8d656e0eb00c52c70eed
Masato Ikeda <masato.ikeda@gmail.com>
2014-08-08 02:12:50 +0900

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source "https://rubygems.org"
+
+gem "rake"
+
+gem "lita"
+gem "lita-twitter"
+gem "twitter"
+
+gem "dotenv"
+
---
+++ lita_config.rb
@@ -0,0 +1,29 @@
+if ENV['RACK_ENV'] !='production'
+  require 'dotenv'
+  Dotenv.load
+end
+
+Dir[File.expand_path("../extensions/*.rb", __FILE__)].each do |file|
+  require file
+end
+
+Lita.deregister_handler(Lita::Handlers::Help)
+Lita.deregister_handler(Lita::Handlers::Info)
+
+Dir[File.expand_path("../handlers/*.rb", __FILE__)].each do |file|
+  require file
+end
+
+Lita.configure do |config|
+  config.robot.name       = 'haruna_bot_'
+  config.robot.adapter    = :twitter
+  config.robot.log_level  = :debug
+
+  config.redis.url = ENV["REDISTOGO_URL"]
+  config.http.port = ENV["PORT"]
+
+  config.adapter.api_key             = ENV['API_KEY']
+  config.adapter.api_secret          = ENV['API_SECRET']
+  config.adapter.access_token        = ENV['ACCESS_TOKEN']
+  config.adapter.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
