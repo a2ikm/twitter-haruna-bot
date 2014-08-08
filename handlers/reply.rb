f89a19225ffb990c175ed9b1b88d0cf663c0ae38
Masato Ikeda <masato.ikeda@gmail.com>
2014-08-09 02:09:21 +0900

    Reply to all questions with all right!

--- handlers/reply.rb
+++ handlers/reply.rb
@@ -1,7 +1,7 @@
 module Lita
   module Handlers
     class Reply < Handler
-      route /大丈夫？/, :all_right!
+      route /[?？]/, :all_right!
       def all_right!(message)
         return unless message.user.metadata["mention"]
         message.reply_with_mention "榛名は大丈夫です！"
