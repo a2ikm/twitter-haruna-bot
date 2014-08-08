aabd2e94441f358c67ba108a5cf046ba05e734c3
Masato Ikeda <masato.ikeda@gmail.com>
2014-08-09 02:05:25 +0900

    Send scheduled messages

---
+++ handlers/left.rb
@@ -0,0 +1,53 @@
+require "rufus-scheduler"
+
+module Lita
+  module Handlers
+    class Left < Handler
+      def self.default_config(handler_config)
+        handler_config.room             = nil
+        handler_config.night_battle_at  = nil
+        handler_config.sleepy_at        = nil
+      end
+      
+      on :loaded do |payload|
+        return unless config.room
+
+        @scheduler = Rufus::Scheduler.new
+
+        if night_battle_at = config.night_battle_at
+          @scheduler.cron night_battle_at do
+            robot.trigger :night_battle, room: config.room
+          end
+        end
+
+        if sleepy_at = config.sleepy_at
+          @scheduler.cron sleepy_at do
+            robot.trigger :sleepy, room: config.room
+          end
+        end
+      end
+
+      on :night_battle do |payload|
+        target = Source.new(room: payload[:room])
+        robot.send_message(target, "夜戦なの？腕がなるわね！")
+      end
+
+      on :sleepy do |payload|
+        target = Source.new(room: payload[:room])
+        messages = [
+          "榛名、待機命令…了解です…。",
+          "いいのでしょうか？榛名がお休みしてて…。",
+        ]
+        robot.send_message(target, messages.sample)
+      end
+
+      private
+
+        def config
+          Lita.config.handlers.left
+        end
+    end
+
+    Lita.register_handler(Left)
+  end
+end
--- lita_config.rb
+++ lita_config.rb
@@ -26,4 +26,8 @@ Lita.configure do |config|
   config.adapter.api_secret          = ENV['API_SECRET']
   config.adapter.access_token        = ENV['ACCESS_TOKEN']
   config.adapter.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+
+  config.handlers.left.room            = 1
+  config.handlers.left.night_battle_at = "0 0 * * * Asia/Tokyo"
+  config.handlers.left.sleepy_at       = "0 #{1.upto(23).to_a.join(",")} * * * Asia/Tokyo"
 end
